36696878bafe2b187eda2ce6c53affadfffa519b
Florin Micu <florinmmicu@gmail.com>
2013-04-09 16:58:20 +0200

    Worked on style

--- Hashes/sdmb.py
+++ Hashes/sdmb.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 
+"""sdbm (database manager) hashing implementation"""
+
 import sys
 from ctypes import c_ulong
 
-def ulong(i): return c_ulong(i).value  # numpy would be better if available
+def ulong(i):
+    """gets the value from c_ulong"""
+
+    return c_ulong(i).value  # numpy would be better if available
 
 def sdbm(to_hash):
     """transforms a string into a hash(long)"""
 
     # hash to return
-    hash = 0
+    ret_hash = 0
 
-    for x in to_hash:
-        hash = ulong(ord(x) + ulong(hash << 6) + ulong(hash << 16) - hash)
-        print "hash: {0}".format(hash)
+    for mchar in to_hash:
+        ret_hash = ulong(ord(mchar) + ulong(ret_hash << 6) + 
+                         ulong(ret_hash << 16) - ret_hash)
+ 
 
-    return hash
+    return ret_hash
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
